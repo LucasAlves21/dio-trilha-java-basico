3cfd9f11f895e2da2d50715cef19f28f4fda60c7
Lucas Alves <Lucasalvesf19@gmail.com>
2024-05-07 00:43:05 -0300

    operadores

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,72 @@
+public class operadores {
+    public static void main(String[] args) throws Exception{
+  /*       // concatenar
+
+        String juntar = "Palavra" + "frase";
+       
+       
+        // aritimeticos! 
+
+        int somar = 10 + 4;
+        int subtração = 10 - 4;
+        int multiplicação = 10 * 4;
+        int divisão = 10 / 4;
+        double modulo = 10 % 4;
+
+        // unarios
+
+        int valorPositivo = -10 * -1;
+        int valorNegativo = - 10;
+        
+
+        // unario incrementação e decremento
+       
+        int addUm = 10;
+        addUm++;
+
+        int tiraUm = 10;
+        tiraUm-- ;
+        
+        // unario de negação ou seja um boolean positivo vira negativo ou vice-versa
+
+        boolean variavel = true;
+        variavel = !variavel;
+       
+        //ternario
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String ternario = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(ternario); 
+
+        // relacional 
+
+        int a = 1;
+        int b = 2;
+
+        boolean simNao;
+
+        simNao = a == b;
+        simNao = a != b;
+        simNao = a > b;
+        simNao = a > b;
+
+        // logicos
+        
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            
+        }
+        
+        if (condicao2 || condicao1 ) {
+        } */
+            
+        
+
+    
+    }
+}
