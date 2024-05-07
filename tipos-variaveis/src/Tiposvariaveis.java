c4f3faf7b6e0f425e6b7bc7c55d2a287d3b9a1cb
Lucas Alves <Lucasalvesf19@gmail.com>
2024-05-06 23:34:12 -0300

    estudo de variaveis

--- tipos-variaveis/src/Tiposvariaveis.java
+++ tipos-variaveis/src/Tiposvariaveis.java
@@ -1,5 +1,14 @@
 public class Tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos 
+
+        String nome = "lucas";
+
+        int quantidadeDeJanelas = 6;
+        double salario = 1000;
+        quantidadeDeJanelas = 6;
+        System.out.print(salario + quantidadeDeJanelas + " ");
+        System.out.print(nome);
     }
+
 }
